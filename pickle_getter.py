3cf4e69b04a0f449f000dcc3642bd6d3422df19d
katriandra <yekaterinagrichshuk@gmail.com>
2022-04-19 16:58:28 +0300

    v1

--- pickle_getter.py
+++ pickle_getter.py
@@ -1,7 +1,12 @@
 import pickle
-import pickle_dumper
 import os
 import argparse
+
+
+class MyClass:
+    def __init__(self, par1, par2):
+        self.par1 = par1
+        self.par2 = par2
 
 
 if os.path.exists('settings/obj.pickle'):
